32c6b5aea297b71d8274243126c7ac9cdd8d34ce
hadley <h.wickham@gmail.com>
2011-12-11 15:16:44 -0600

    More 1d binning tests

--- inst/tests/test-1d-bin.r
+++ inst/tests/test-1d-bin.r
@@ -1,11 +1,33 @@
-
+context("1d binning")
 # compare results of bin_1d("dataNA", "x", bin) and bin_1d(data_NA, "x", bin)
 
-test_that("bin_1d equivalent to table", {
-  x <- sample(100, 1000, rep = T)
-  tbl <- c(table(x)[], 0)
+test_that("binning integers equivalent to table", {
+  x <- c(0, 100, sample(100, 1000, rep = TRUE))
   bin <- bin_1d(data.frame(x), "x", 1)
-  expect_that(tbl, equals(bin$Counts))  
+  expect_that(bin$range, equals(c(0, 100)))
+
+  tbl <- c(table(x)[], 0)
+  expect_that(tbl, equals(bin$count))
+})
+
+test_that("binning reals equivalent to findInterval + tabulate", {
+  x <- c(-10, 10, rnorm(1000))
+  bin <- bin_1d(data.frame(x), "x", 1)
+  expect_that(bin$range, equals(c(-10, 10)))
+  
+  bins <- findInterval(x, seq(-10, 10))
+  tbl <- c(tabulate(bins), 0)
+  expect_that(tbl, equals(bin$count))  
+})
+
+test_that("missing values counted correct", {
+  x <- 1:10
+  bin <- bin_1d(data.frame(x), "x", 1)
+  expect_that(bin$count[[11]], equals(0))
+  
+  x <- c(1:10, NA)
+  bin <- bin_1d(data.frame(x), "x", 1)
+  expect_that(bin$count[[11]], equals(1))
 })
 
 # Comparisons between data frames and xdfs -----------------------------------
