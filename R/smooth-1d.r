f5549c4f3dc7911c86cee6879a2bd51d84b52edf
hadley <h.wickham@gmail.com>
2013-01-24 09:51:24 -0600

    Get smooth1d working

--- R/smooth-1d.r
+++ R/smooth-1d.r
@@ -1,11 +1,22 @@
-smooth_1d <- function(summary, var = names(summary)[2], kernel = kernel("norm", sd = 1),
-  grid = NULL, n = nrow(summary), reflect = TRUE) {
-  stopifnot(is.kernel(kernel))
-
+#' Smooth 1d summary statistics
+#'
+#' @param summary summary statistics produced by \code{\link{summary1d}}
+#' @param bw smoothing bandwidth/standard deviation
+#' @param var variable to smooth
+#' @param grid,n A grid of locations to produced smoothed values. If \code{NULL},
+#'   the default, will be an evenly spaced grid of \code{n} points across the
+#'   the range of \code{x}.
+#' @examples
+#' x <- runif(1e5)
+#' xsum <- summarise1d(x, binwidth = 1/100)
+#' xsmu <- smooth1d(xsum, 1/100)
+#'
+#' plot(xsum)
+#' lines(xsmu)
+smooth1d <- function(summary, bw, var = names(summary)[2], grid = NULL, n = nrow(summary), reflect = TRUE) {
   x_rng <- frange(summary$x)
 
   if (is.null(grid)) {
-
     if (reflect) {
       g_rng <- x_rng
     } else {
@@ -17,41 +28,7 @@ smooth_1d <- function(summary, var = names(summary)[2], kernel = kernel("norm", 
     grid <- seq(g_rng[1], g_rng[2], length = n)
   }
 
-  convolve_1d(summary$x, summary[var], grid, kernel, reflect = reflect)
-
-  # Generate the kernel
-  l <- trunc(bandwidth / binwidth) * 4L
-  if (l < 1) {
-    warning("Bandwidth too small: no smoothing", call. = FALSE)
-    return(summary)
-  }
-
-  grid <- seq(-4, 4, length = 2L * l + 1L)
-  kernel <- dnorm(grid, sd = bandwidth)
-
-  # Smooth, by convolving data with kernel
-  smooth <- array(convolve_1d(counts$data, kernel))
-
-  counts$data <- smooth
-  counts$centers[[1]] <- expand_centers(counts$centers[[1]], l, binwidth)
-  counts
+  no_na <- summary[-1, ]
+  s <- smooth_1d_normal(x = no_na$x[-1], z = no_na[[var]], x_out = grid, sd = bw)
+  data.frame(x = c(NA, grid), s = c(summary[[var]][1], s))
 }
-
-#' A trimmed kernel function.
-#'
-#' The resulting kernel will not integrate to 1: the assumption is that the
-#' mass outside the range is negligible. This is done for efficiency reasons.
-kernel <- function(dist, ..., range = c(0.005, 0.995)) {
-  stopifnot(is.character(dist), length(dist) == 1)
-  qdist <- match.fun(paste("q", dist, sep = ""))
-  ddist <- match.fun(paste("d", dist, sep = ""))
-
-  krange <- qdist(range, ...)
-
-  structure(function(x) ddist(x, ...), class = "kernel")
-}
-
-is.kernel <- function(x) inherits(x, "kernel")
-range.kernel <- function(x, ...) environment(x)$krange
-
-
