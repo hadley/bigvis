9a313148f29e2bf387d25608095e6e1fbe9cc37b
hadley <h.wickham@gmail.com>
2013-02-05 16:19:37 -0600

    Fix rebin arguments

--- R/rebin.r
+++ R/rebin.r
@@ -15,43 +15,44 @@ rebin.binsum_median <- function(x) {
   if (!anyDuplicated(x)) return(x)
   message("Warning: can not rebin medians. Approximating using mean.")
 
-  x <- unique(df$x)
-  x_id <- match(df$x, x)
-  xs <- split(seq_len(nrow(df)), x_id)
+  ux <- unique(x$x)
+  x_id <- match(x$x, ux)
+  xs <- split(seq_len(nrow(x)), x_id)
 
-  out <- data.frame(x = unique(df$x))
-  out$median <- grp_apply(xs, function(i) mean(df$median[i], na.rm = TRUE))
+  out <- data.frame(x = ux)
+  out$median <- grp_apply(xs, function(i) mean(x$median[i], na.rm = TRUE))
   binsum(out, type = type(x))
 }
 
 rebin.binsum_sum <- function(x) {
   if (!anyDuplicated(x)) return(x)
 
-  x <- unique(df$x)
-  x_id <- match(df$x, x)
-  xs <- split(seq_len(nrow(df)), x_id)
 
-  out <- data.frame(x = unique(df$x))
-  out$count <- grp_apply(xs, function(i) sum(df$count[i]))
-  if (!is.null(df$mean)) {
-    out$sum <- grp_apply(xs, function(i) sum(df$sum[i]))
+  ux <- unique(x$x)
+  x_id <- match(x$x, ux)
+  xs <- split(seq_len(nrow(x)), x_id)
+
+  out <- data.frame(x = ux)
+  out$count <- grp_apply(xs, function(i) sum(x$count[i]))
+  if (!is.null(x$mean)) {
+    out$sum <- grp_apply(xs, function(i) sum(x$sum[i]))
   }
 
   binsum(out, type = type(x))
 }
 
 rebin.binsum_moments <- function(x) {
-  if (!anyDuplicated(x)) return(x)
+  if (!anyDuplicated(x$x)) return(x)
 
-  x <- unique(df$x)
-  x_id <- match(df$x, x)
-  xs <- split(seq_len(nrow(df)), x_id)
+  ux <- unique(x$x)
+  x_id <- match(x$x, ux)
+  xs <- split(seq_len(nrow(x)), x_id)
 
-  out <- data.frame(x = unique(df$x))
+  out <- data.frame(x = ux)
 
-  out$count <- grp_apply(xs, function(i) sum(df$count[i]))
-  if (!is.null(df$mean)) {
-    out$mean <- grp_apply(xs, function(i) weighted.mean(df$mean[i], df$count[i]))
+  out$count <- grp_apply(xs, function(i) sum(x$count[i]))
+  if (!is.null(x$mean)) {
+    out$mean <- grp_apply(xs, function(i) weighted.mean(x$mean[i], x$count[i]))
   }
 
   binsum(out, type = type(x))
