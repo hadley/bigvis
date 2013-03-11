87ba14b3d9b4dfb8ffc0d221030e2b34b5791f34
hadley <h.wickham@gmail.com>
2013-03-11 14:43:59 -0400

    Better input validation & docs for bin

--- R/bin.r
+++ R/bin.r
@@ -7,9 +7,10 @@
 #' \code{\link{condense}} and friends.
 #'
 #' @param x numeric or integer vector
-#' @param width bin width
-#' @param origin if not specified, guessed by \code{\link{find_origin}}
-#' @param name name of original variable. This will be guess from the input to
+#' @param width bin width. If not specified, about 10,000 bins will be chosen
+#'   using the algorithim in \code{\link{find_width}}.
+#' @param origin origin. If not specified, guessed by \code{\link{find_origin}}.
+#' @param name name of original variable. This will be guessed from the input to
 #'   \code{group} if not supplied. Used in the output of
 #'   \code{\link{condense}} etc.
 #' @export
@@ -17,15 +18,25 @@
 #' x <- runif(1e6)
 #' bin(x)
 #' bin(x, 0.01)
+#' bin(x, 0.01, origin = 0.5)
 bin <- function(x, width = find_width(x), origin = find_origin(x, width),
                 name = NULL) {
+  stopifnot(is.numeric(x))
+  stopifnot(is.numeric(width), length(width) == 1, width > 0)
+  stopifnot(is.numeric(origin), length(origin) == 1)
+
   if (is.null(name)) {
     name <- deparse(substitute(x))
   }
+  stopifnot(is.character(name), length(name) == 1)
 
   if (!is.ranged(x)) {
     attr(x, "range") <- frange(x)
     class(x) <- "ranged"
+  }
+  if (origin > min(x)) {
+    warning("Origin larger than min(x): some values will be truncated",
+      call. = FALSE)
   }
 
   BigVis$BinnedVector$new(x, name, width, origin)
